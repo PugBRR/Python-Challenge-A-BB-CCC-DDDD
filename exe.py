3846375df324b31d61a0693844a72cc96af0bb59
Basso <nbasso32@gmail.com>
2022-01-30 12:01:36 -0300

    Challenge
    Update

---
+++ exe.py
@@ -0,0 +1,21 @@
+#==============================================#
+# Desafio / Challenge: a, bb, ccc, dddd        #
+# Nome: Nicolas Name: Nicolas                  #
+# Email: nbasso32@gmail.com                    #
+#==============================================#
+
+list = []
+
+for p in range(1, 5):
+    list.append(chr(p + 96) * p)
+
+for i in list:
+    print(i)
+
+#==============================================#
+#Saída / Output:
+# a
+# bb
+# ccc
+# dddd
+#==============================================#
